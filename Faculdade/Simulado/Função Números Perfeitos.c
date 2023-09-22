1a3f588b30e00fe5839b0eaeba1d78e1270f1320
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2023-09-22 17:52:06 -0300

    Função Números Perfeitos.c

--- Faculdade/Simulado/Função Números Perfeitos.c
+++ Faculdade/Simulado/Função Números Perfeitos.c
@@ -1,3 +1,13 @@
+/* Construa um programa em C ANSI onde exista uma função que recebe por parâmetro um número e
+retorne 1 caso ele seja perfeito ou zero caso ele não seja perfeito. Um número perfeito é um número que a
+soma de seus divisores com exceção do próprio número é igual a ele mesmo. Ex: 6 ( 1 + 2 + 3 ), 28 (
+1+2+4+7+14). Na sua função principal, solicite ao usuário 10 números e mostre na tela:
+a) Quais números são perfeitos;
+b) Quantos são perfeitos;
+c) O maior número perfeito;
+d) O menor número perfeito;
+e) Somatório dos números perfeitos. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
