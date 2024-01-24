9aedb813b791e87b0e381ea1513ef525feb28ac6
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-01-24 15:48:24 -0300

    Create Abaixo da Diagonal Principal.c

---
+++ Beecrowd/Abaixo da Diagonal Principal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  double M[12][12];
+  int i, j, elementos = 0;
+  double Resultado = 0.0;
+  char O;
+
+  scanf("%c", &O);
+
+  for (i = 0; i < 12; i++) {
+    for (j = 0; j < 12; j++) {
+      scanf("%lf", &M[i][j]);
+    }
+  }
+
+  for (i = 0; i < 12; i++) {
+    for (j = 0; j < i; j++) {
+      Resultado += M[i][j];
+      elementos++;
+    }
+  }
+
+  if (O == 'S') {
+    printf("%.1f\n", Resultado);
+  } else if (O == 'M') {
+    printf("%.1lf\n", Resultado / elementos);
+  }
+  return 0;
+}
